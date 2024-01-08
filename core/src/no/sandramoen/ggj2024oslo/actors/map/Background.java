b0a9af4092af4483539f71c6603f4a293966902a
Sandra Moen <sandramoen01@gmail.com>
2024-01-08 10:31:22 +0100

    initial commit

---
+++ core/src/no/sandramoen/ggj2024oslo/actors/map/Background.java
@@ -0,0 +1,21 @@
+package no.sandramoen.ggj2024oslo.actors.map;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+
+import no.sandramoen.ggj2024oslo.actors.utils.BaseActor;
+import no.sandramoen.ggj2024oslo.utils.BaseGame;
+
+public class Background extends BaseActor {
+    private Array<TextureAtlas.AtlasRegion> animationImages = new Array();
+
+    public Background(float x, float y, Stage stage) {
+        super(x, y, stage);
+        animationImages.add(BaseGame.textureAtlas.findRegion("grass"));
+        animation = new Animation(2f, animationImages, Animation.PlayMode.LOOP);
+        setAnimation(animation);/*
+        setSize(TiledMapActor.mapWidth, TiledMapActor.mapHeight);*/
+    }
+}
