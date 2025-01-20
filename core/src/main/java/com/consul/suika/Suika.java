1a8bffaa0b3d748d002b13d9c9460e6d28c17548
Migihhhh <miggy.consul@gmail.com>
2025-01-20 17:28:01 +0800

    added physics and spawning of 'flower' (pls fix the spawn position of it)
    added the box so flowers cant go outside.
    
    needed: collision for merging of fruits, scoring system, and sprites.

--- core/src/main/java/com/consul/suika/Suika.java
+++ core/src/main/java/com/consul/suika/Suika.java
@@ -3,19 +3,31 @@ package com.consul.suika;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+
 
 public class Suika implements ApplicationListener {
     Texture player;
     Texture background;
+
+    Texture fruit;
 
     ExtendViewport viewport;
 
@@ -23,7 +35,7 @@ public class Suika implements ApplicationListener {
 
 
     Sprite playerSprite;
-
+    Sprite fruitSprite;
     Vector2  touchPos;
 
     Rectangle playerHitbox;
@@ -31,32 +43,43 @@ public class Suika implements ApplicationListener {
     Rectangle leftLine;
     Rectangle rightLine;
 
-
     private final int WORLD_WIDTH = 110;
     private final int WORLD_HEIGHT = 192;
+
+    Box2DDebugRenderer debugRenderer;
+    OrthographicCamera camera;
+
+    World world;
+    Body player2D;
 
 
     @Override
     public void create() {
         player = new Texture("player.png");
         background = new Texture("gameBackground.png");
+        fruit = new Texture("fruit.png");
 
         batch = new SpriteBatch();
         viewport = new ExtendViewport(86, 165);
 
         playerSprite = new Sprite(player);
+        fruitSprite = new Sprite(fruit);
 
         playerSprite.setY(110f);
         playerSprite.setX(43f);
 
         playerSprite.setSize(15,15);
-
-
-
-
+        fruitSprite.setSize(6,6);
 
 
         touchPos  = new Vector2();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 86, 165);
+
+        world = new World(new Vector2(0, -15f), false);
+        debugRenderer = new Box2DDebugRenderer();
+
 
 
 
@@ -66,27 +89,32 @@ public class Suika implements ApplicationListener {
     public void resize(int width, int height) {
         viewport.update(width, height, true);
         batch.setProjectionMatrix(viewport.getCamera().combined);
-
-
+        camera.setToOrtho(false, 86, 165);
 
     }
 
     @Override
     public void render() {
+
+
         input();
         logic();
         draw();
 
-
+        createPlatform(17.5f,55.5f,1,45);
+        createPlatform(68.5f,55.5f,1,45);
+        createPlatform(43,8.5f,25,1);
+        update(Gdx.graphics.getDeltaTime());
+        debugRenderer.render(world, camera.combined);
     }
 
     private void input() {
-        if(Gdx.input.isTouched()){
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            playerSprite.setCenterX(touchPos.x);
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(touchPos);
+        playerSprite.setCenterX(touchPos.x);
 
-
+        if(Gdx.input.justTouched()){
+            createFlower();
         }
 
     }
@@ -94,7 +122,8 @@ public class Suika implements ApplicationListener {
     private void logic() {
 
         playerHitbox = new Rectangle(playerSprite.getX(), playerSprite.getY(), playerSprite.getWidth(), playerSprite.getHeight());
-        playerSprite.setX(MathUtils.clamp(playerSprite.getX(), 21, 92.5f - playerSprite.getWidth()));
+        playerSprite.setX(MathUtils.clamp(playerSprite.getX(), 23f, 96f - playerSprite.getWidth()));
+        fruitSprite.setX(MathUtils.clamp(playerSprite.getX(), 23f, 96f - playerSprite.getWidth()));
 
     }
 
@@ -111,7 +140,51 @@ public class Suika implements ApplicationListener {
         playerSprite.draw(batch);
 
 
+
+
+
+
         batch.end();
+    }
+
+    public Body createFlower(){
+        Body pBody;
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.DynamicBody;
+        def.position.set(playerSprite.getX(), playerSprite.getY());
+        pBody = world.createBody(def);
+
+        CircleShape circle = new CircleShape();
+        circle.setRadius(2f);
+
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.11f; // Make it bounce a little bit
+
+        Fixture fixture = pBody.createFixture(fixtureDef);
+
+
+
+        return pBody;
+    }
+
+    public Body createPlatform(float posx, float posy, float width, float height){
+        Body ground;
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.StaticBody;
+        def.position.set(posx,posy);
+        ground = world.createBody(def);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width,height);
+
+        ground.createFixture(shape,1);
+        shape.dispose();
+
+        return ground;
     }
 
     @Override
@@ -126,6 +199,16 @@ public class Suika implements ApplicationListener {
 
     @Override
     public void dispose() {
+        debugRenderer.dispose();
+        world.dispose();
+        batch.dispose();
+        player.dispose();
+        background.dispose();
+        fruit.dispose();
 
+    }
+
+    public void update(float delta){
+        world.step(1/60f,6,2);
     }
 }
