4242c7b020b5cd15a08a7be6bd5b3634afe94abc
Migihhhh <miggy.consul@gmail.com>
2025-02-23 15:17:47 +0800

    main menu added, game works, sound and music is here TODO: fix game on android

--- core/src/main/java/no/sandramoen/ggj2024oslo/actors/components/PlayerControlComponent.java
+++
@@ -1,5 +0,0 @@
-package no.sandramoen.ggj2024oslo.actors.components;
-
-import com.badlogic.ashley.core.Component;
-
-public class PlayerControlComponent implements Component {}
--- core/src/main/java/no/sandramoen/ggj2024oslo/actors/particles/RedKeyEffect.java
+++
@@ -1,7 +0,0 @@
-package no.sandramoen.ggj2024oslo.actors.particles;
-
-public class RedKeyEffect extends ParticleActor{
-    public RedKeyEffect() {
-        super("effects/RedKeyEffect.pfx");
-    }
-}
